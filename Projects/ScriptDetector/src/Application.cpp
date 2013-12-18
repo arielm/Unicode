5cc0b0d16a8192537056d02b9ec20c2803c50c9f
Ariel Malka <ariel@chronotext.org>
2013-12-18 16:59:55 +0200

    ScriptDetector: USING std::string AS INPUT

--- Projects/ScriptDetector/src/Application.cpp
+++ Projects/ScriptDetector/src/Application.cpp
@@ -13,31 +13,18 @@
  * TODO:
  *
  * 1) COMPILE ON ANDROID AND iOS:
- *    - CHECK IF THE DEFINES (SEE IN Prefix.pch) HELP ON ANDROID
- *
- * 2) USE std::string
+ *    SEE THE DEFINES IN Prefix.pch
  */
 
 #include "cinder/app/AppNative.h"
+
+#include "unicode/unistr.h"
 
 #include "scrptrun.h"
 
 using namespace std;
 using namespace ci;
 using namespace app;
-
-UChar testChars[] =
-{
-    0x0020, 0x0946, 0x0939, 0x093F, 0x0928, 0x094D, 0x0926, 0x0940, 0x0020,
-    0x0627, 0x0644, 0x0639, 0x0631, 0x0628, 0x064A, 0x0629, 0x0020,
-    0x0420, 0x0443, 0x0441, 0x0441, 0x043A, 0x0438, 0x0439, 0x0020,
-    'E', 'n', 'g', 'l', 'i', 's', 'h',  0x0020,
-    0x6F22, 0x5B75, 0x3068, 0x3072, 0x3089, 0x304C, 0x306A, 0x3068,
-    0x30AB, 0x30BF, 0x30AB, 0x30CA,
-    0xD801, 0xDC00, 0xD801, 0xDC01, 0xD801, 0xDC02, 0xD801, 0xDC03
-};
-
-int32_t testLength = sizeof testChars / sizeof testChars[0];
 
 class Application : public AppNative
 {
@@ -48,13 +35,16 @@ public:
 
 void Application::setup()
 {
-    ScriptRun scriptRun(testChars, 0, testLength);
+    const string input = " ॆहिन्दी العربية Русский English 漢孵とひらがなとカタカナ𐐀𐐁𐐂𐐃";
+    
+    UnicodeString tmp = UnicodeString::fromUTF8(input);
+    ScriptRun scriptRun(tmp.getBuffer(), tmp.length());
     
     while (scriptRun.next())
     {
-        int32_t     start = scriptRun.getScriptStart();
-        int32_t     end   = scriptRun.getScriptEnd();
-        UScriptCode code  = scriptRun.getScriptCode();
+        auto start = scriptRun.getScriptStart();
+        auto end = scriptRun.getScriptEnd();
+        auto code = scriptRun.getScriptCode();
         
         printf("Script '%s' from %d to %d.\n", uscript_getName(code), start, end);
     }
