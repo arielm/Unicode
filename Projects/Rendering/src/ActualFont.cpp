arielm/Unicode

commit 604c7abc2d43c4e2f123fdd5fbd431e034d18904

Rendering: POSSIBILITY TO UNLOAD/RELOAD ActualFont INSTANCES

Projects/Rendering/src/ActualFont.cpp
@@ -8,9 +8,9 @@
 
 #include "ActualFont.h"
 
-#include "hb-ft.h"
+#include "chronotext/utils/Utils.h"
 
-#include "cinder/Utilities.h"
+#include "hb-ft.h"
 
 using namespace std;
 using namespace ci;
@@ -40,136 +40,116 @@ static FT_Error force_ucs2_charmap(FT_Face face)
     return -1;
 }
 
-ActualFont::ActualFont(shared_ptr<FreetypeHelper> ftHelper, InputSourceRef source, float baseSize, bool useMipmap, int padding)
+ActualFont::ActualFont(shared_ptr<FreetypeHelper> ftHelper, InputSourceRef inputSource, float baseSize, bool useMipmap, int padding)
 :
 ftHelper(ftHelper),
+inputSource(inputSource),
 baseSize(baseSize),
 useMipmap(useMipmap),
-padding(padding)
+padding(padding),
+loaded(false),
+ftFace(NULL),
+hbFont(NULL)
 {
-    if (!source->isFile())
-    {
-        throw runtime_error("ActualFont MUST BE CREATED FROM FILE");
-    }
-    
-    FT_Error error = FT_New_Face(ftHelper->getLib(), source->getFilePath().c_str(), 0, &ftFace);
-    
-    if (error)
-    {
-        throw runtime_error("FREETYPE: ERROR " + toString(error));
-    }
-    
-    if (force_ucs2_charmap(ftFace))
-    {
-        throw runtime_error("HARFBUZZ: FONT IS BROKEN OR IRRELEVANT");
-    }
-    
-    // ---
-    
-    /*
-     * USING A MATRIX WITH A MULTIPLIER ALLOWS FOR FRACTIONAL VALUES
-     * TRICK FROM http://code.google.com/p/freetype-gl/
-     *
-     * - WITHOUT A FRACTIONAL ADVANCE: CHARACTER SPACING LOOKS DUMB
-     * - WITHOUT A FRACTIONAL HEIGHT: SOME CHARACTERS WON'T BE PERFECTLY ALIGNED ON THE BASELINE
-     */
-    int res = 64;
-    int dpi = 72;
-    
-    scale = Vec2f::one() / Vec2f(res, res) / 64;
-    FT_Set_Char_Size(ftFace, baseSize * 64, 0, dpi * res, dpi * res);
-    
-    FT_Matrix matrix =
-    {
-        int((1.0 / res) * 0x10000L),
-        int((0.0) * 0x10000L),
-        int((0.0) * 0x10000L),
-        int((1.0 / res) * 0x10000L)
-    };
-    
-    FT_Set_Transform(ftFace, &matrix, NULL);
-    
-    // ---
-    
-    metrics.height = ftFace->size->metrics.height * scale.y;
-    metrics.ascent = ftFace->size->metrics.ascender * scale.y;
-    metrics.descent = -ftFace->size->metrics.descender * scale.y;
-
-    metrics.lineThickness = ftFace->underline_thickness / 64.0f;
-    metrics.underlineOffset = -ftFace->underline_position / 64.0f;
-    
-    //
-    
-    auto os2 = (TT_OS2*)FT_Get_Sfnt_Table(ftFace, ft_sfnt_os2);
-    
-    if (os2 && (os2->version != 0xFFFF))
-    {
-        metrics.strikethroughOffset = FT_MulFix(os2->yStrikeoutPosition, ftFace->size->metrics.y_scale) * scale.y;
-    }
-    else
-    {
-        metrics.strikethroughOffset = 0.5f * (metrics.ascent - metrics.descent);
-    }
-    
-    // ---
-    
-    hbFont = hb_ft_font_create(ftFace, NULL);
+    reload();
 }
 
 ActualFont::~ActualFont()
 {
-    hb_font_destroy(hbFont);
-    FT_Done_Face(ftFace);
+    unload();
 }
 
-ActualFont::Glyph* ActualFont::getGlyph(uint32_t codepoint)
+void ActualFont::reload()
 {
-    Glyph *glyph = NULL;
-    auto entry = glyphCache.find(codepoint);
-    
-    if (entry == glyphCache.end())
+    if (!loaded)
     {
-        glyph = createGlyph(codepoint);
-        
-        if (glyph)
+        if (!inputSource->isFile())
         {
-            glyphCache[codepoint] = unique_ptr<Glyph>(glyph);
+            throw runtime_error("ActualFont MUST BE CREATED FROM FILE");
+        }
+        
+        FT_Error error = FT_New_Face(ftHelper->getLib(), inputSource->getFilePath().c_str(), 0, &ftFace);
+        
+        if (error)
+        {
+            throw runtime_error("FREETYPE: ERROR " + toString(error));
+        }
+        
+        if (force_ucs2_charmap(ftFace))
+        {
+            FT_Done_Face(ftFace);
+            throw runtime_error("HARFBUZZ: FONT IS BROKEN OR IRRELEVANT");
+        }
+        
+        hbFont = hb_ft_font_create(ftFace, NULL);
+
+        // ---
+        
+        /*
+         * USING A MATRIX WITH A MULTIPLIER ALLOWS FOR FRACTIONAL VALUES
+         * TRICK FROM http://code.google.com/p/freetype-gl/
+         *
+         * - WITHOUT A FRACTIONAL ADVANCE: CHARACTER SPACING LOOKS DUMB
+         * - WITHOUT A FRACTIONAL HEIGHT: SOME CHARACTERS WON'T BE PERFECTLY ALIGNED ON THE BASELINE
+         */
+        int res = 64;
+        int dpi = 72;
+        
+        scale = Vec2f::one() / Vec2f(res, res) / 64;
+        FT_Set_Char_Size(ftFace, baseSize * 64, 0, dpi * res, dpi * res);
+        
+        FT_Matrix matrix =
+        {
+            int((1.0 / res) * 0x10000L),
+            int((0.0) * 0x10000L),
+            int((0.0) * 0x10000L),
+            int((1.0 / res) * 0x10000L)
+        };
+        
+        FT_Set_Transform(ftFace, &matrix, NULL);
+        
+        // ---
+        
+        metrics.height = ftFace->size->metrics.height * scale.y;
+        metrics.ascent = ftFace->size->metrics.ascender * scale.y;
+        metrics.descent = -ftFace->size->metrics.descender * scale.y;
+        
+        metrics.lineThickness = ftFace->underline_thickness / 64.0f;
+        metrics.underlineOffset = -ftFace->underline_position / 64.0f;
+        
+        //
+        
+        auto os2 = (TT_OS2*)FT_Get_Sfnt_Table(ftFace, ft_sfnt_os2);
+        
+        if (os2 && (os2->version != 0xFFFF))
+        {
+            metrics.strikethroughOffset = FT_MulFix(os2->yStrikeoutPosition, ftFace->size->metrics.y_scale) * scale.y;
         }
         else
         {
-            /*
-             * INSERTING A VALUE FOR TEXTURELESS GLYPHS (E.G. A "SPACE")
-             * IS NECESSARY, OTHERWISE createGlyph() WOULD BE INVOKED
-             * INDEFINITELY FOR THE SAME CODEPOINT
-             */
-            glyphCache[codepoint] = unique_ptr<Glyph>(new Glyph());
+            metrics.strikethroughOffset = 0.5f * (metrics.ascent - metrics.descent);
         }
-    }
-    else
-    {
-       glyph = entry->second.get();
         
-        /*
-         * IN CASE A PREVIOUSLY-LOADED TEXTURE HAVE BEEN
-         * DISCARDED, E.G. AFTER SOME OPENGL CONTEXT-LOSS
-         */
-        if (glyph->texture && !glyph->texture->isLoaded())
-        {
-            GlyphData glyphData(ftFace, codepoint, useMipmap, padding);
-            
-            if (glyphData.isValid())
-            {
-                glyph->texture->load(glyphData);
-            }
-        }
+        // ---
+        
+        loaded = true;
+        LOGD << "LOADING FONT: " << ftFace->family_name << " "  << ftFace->style_name << endl;
     }
-    
-    return glyph;
 }
 
-void ActualFont::clearGlyphCache()
+void ActualFont::unload()
 {
-    glyphCache.clear();
+    if (loaded)
+    {
+        loaded = false;
+        LOGD << "UNLOADING FONT: " << ftFace->family_name << " "  << ftFace->style_name << endl;
+
+        unloadTextures();
+        clearGlyphCache();
+
+        hb_font_destroy(hbFont);
+        FT_Done_Face(ftFace);
+    }
 }
 
 void ActualFont::unloadTextures()
@@ -178,6 +158,60 @@ void ActualFont::unloadTextures()
     {
         texture->unload();
     }
+}
+
+void ActualFont::clearGlyphCache()
+{
+    glyphCache.clear();
+}
+
+ActualFont::Glyph* ActualFont::getGlyph(uint32_t codepoint)
+{
+    Glyph *glyph = NULL;
+    
+    if (loaded)
+    {
+        auto entry = glyphCache.find(codepoint);
+        
+        if (entry == glyphCache.end())
+        {
+            glyph = createGlyph(codepoint);
+            
+            if (glyph)
+            {
+                glyphCache[codepoint] = unique_ptr<Glyph>(glyph);
+            }
+            else
+            {
+                /*
+                 * INSERTING A VALUE FOR TEXTURELESS GLYPHS (E.G. A "SPACE")
+                 * IS NECESSARY, OTHERWISE createGlyph() WOULD BE INVOKED
+                 * INDEFINITELY FOR THE SAME CODEPOINT
+                 */
+                glyphCache[codepoint] = unique_ptr<Glyph>(new Glyph());
+            }
+        }
+        else
+        {
+            glyph = entry->second.get();
+            
+            /*
+             * IN CASE A PREVIOUSLY-LOADED TEXTURE HAVE BEEN
+             * DISCARDED, E.G. AFTER SOME OPENGL CONTEXT-LOSS
+             */
+            if (glyph->texture && !glyph->texture->isLoaded())
+            {
+                GlyphData glyphData(ftFace, codepoint, useMipmap, padding);
+                
+                if (glyphData.isValid())
+                {
+                    glyph->texture->load(glyphData);
+                }
+            }
+        }
+    }
+    
+    return glyph;
 }
 
 ActualFont::Glyph* ActualFont::createGlyph(uint32_t codepoint)
@@ -191,8 +225,6 @@ ActualFont::Glyph* ActualFont::createGlyph(uint32_t codepoint)
         
         return new Glyph(texture, glyphData.offset, glyphData.size);
     }
-    else
-    {
-        return NULL;
-    }
+    
+    return NULL;
 }

Projects/Rendering/src/Sketch.h
@@ -10,10 +10,24 @@
  * DONE:
  *
  * 1) ActualFont: ENHANCING getGlyph() AND createGlyph()
+ *
+ * 2) POSSIBILITY TO UNLOAD/RELOAD ActualFont INSTANCES:
+ *    - UPON UNLOADING, THE MEMORY IS EMPTIED, BUT THE ActualFont INSTANCE
+ *      AND ITS POINTERS (E.G. INSIDE Cluster INSTANCES) WILL BE PRESERVED
+ *    - DURING THE TIME AN ActualFont IS UNLOADED:
+ *      CREATING LAYOUTS OR DRAWING THEM WILL BE A NO-OP
  */
 
 /*
  * TODO:
+ *
+ * 0) POSSIBILITY TO UNLOAD/RELOAD VirtualFont
+ *    - IMPLIES A SYSTEM FOR UNLOADING/RELOADING "RELATED" ActualFont INSTANCE
+ *      (PROVIDING THEY'RE NOT SHARED WITH OTHER VirtualFont INSTANCES)
+ *
+ * 0) ActualFont: POSSIBILITY TO LOAD FROM MEMORY...
+ *
+ * 0) VirtualFont: ROBUST XML LOADING...
  *
  * 1) ADJUST FONTS:
  *    - iOS:

Projects/Rendering/src/ActualFont.h
@@ -59,27 +59,35 @@ public:
         float lineThickness;
     };
 
-    float baseSize;
-    bool useMipmap;
-    int padding;
-    
     ci::Vec2f scale;
     Metrics metrics;
-    hb_font_t *hbFont;
     
-    ActualFont(std::shared_ptr<FreetypeHelper> ftHelper, chr::InputSourceRef source, float baseSize, bool useMipmap, int padding);
+    ActualFont(std::shared_ptr<FreetypeHelper> ftHelper, chr::InputSourceRef inputSource, float baseSize, bool useMipmap, int padding);
     ~ActualFont();
+
+    void reload();
+    void unload();
     
-    Glyph* getGlyph(uint32_t codepoint);
-    void clearGlyphCache();
     void unloadTextures();
+    void clearGlyphCache();
+    Glyph* getGlyph(uint32_t codepoint);
     
 protected:
     std::shared_ptr<FreetypeHelper> ftHelper;
+
+    chr::InputSourceRef inputSource;
+    float baseSize;
+    bool useMipmap;
+    int padding;
+
+    bool loaded;
     FT_Face ftFace;
+    hb_font_t *hbFont;
     
     std::map<uint32_t, std::unique_ptr<Glyph>> glyphCache;
     std::vector<std::unique_ptr<ReloadableTexture>> standaloneTextures;
     
     Glyph* createGlyph(uint32_t codepoint);
+    
+    friend class VirtualFont;
 };

Projects/Rendering/src/FontManager.cpp
@@ -9,9 +9,9 @@
 #include "FontManager.h"
 
 #include "chronotext/InputSource.h"
+#include "chronotext/utils/Utils.h"
 
 #include "cinder/Xml.h"
-#include "cinder/Utilities.h"
 
 using namespace std;
 using namespace ci;
@@ -41,7 +41,9 @@ ActualFont* FontManager::getActualFont(const string &uri, float baseSize, bool u
             return font;
         }
         catch (exception &e)
-        {}
+        {
+            LOGD << e.what() << " - " << uri << endl;
+        }
         
         return NULL;
     }

Projects/Rendering/src/VirtualFont.cpp
@@ -97,55 +97,58 @@ LineLayout* VirtualFont::createLineLayout(const TextLine &line)
     {
         clusterMap.clear();
         
-        for (auto &font : getFontSet(run.language))
+        for (auto font : getFontSet(run.language))
         {
-            run.apply(line.text, buffer);
-            hb_shape(font->hbFont, buffer, NULL, 0);
-            
-            auto glyphCount = hb_buffer_get_length(buffer);
-            auto glyphInfos = hb_buffer_get_glyph_infos(buffer, NULL);
-            auto glyphPositions = hb_buffer_get_glyph_positions(buffer, NULL);
-            
-            bool hasMissingGlyphs = false;
-            
-            for (int i = 0; i < glyphCount; i++)
+            if (font->hbFont)
             {
-                auto codepoint = glyphInfos[i].codepoint;
-                auto cluster = glyphInfos[i].cluster;
+                run.apply(line.text, buffer);
+                hb_shape(font->hbFont, buffer, NULL, 0);
                 
-                auto it = clusterMap.find(cluster);
-                bool clusterFound = (it != clusterMap.end());
+                auto glyphCount = hb_buffer_get_length(buffer);
+                auto glyphInfos = hb_buffer_get_glyph_infos(buffer, NULL);
+                auto glyphPositions = hb_buffer_get_glyph_positions(buffer, NULL);
                 
-                if (codepoint)
+                bool hasMissingGlyphs = false;
+                
+                for (int i = 0; i < glyphCount; i++)
                 {
-                    if (clusterFound && (it->second.font != font))
+                    auto codepoint = glyphInfos[i].codepoint;
+                    auto cluster = glyphInfos[i].cluster;
+                    
+                    auto it = clusterMap.find(cluster);
+                    bool clusterFound = (it != clusterMap.end());
+                    
+                    if (codepoint)
                     {
-                        continue; // CLUSTER FOUND, WITH ANOTHER FONT (E.G. SPACE)
-                    }
-                    else
-                    {
-                        auto offset = Vec2f(glyphPositions[i].x_offset, -glyphPositions[i].y_offset) * font->scale;
-                        float advance = glyphPositions[i].x_advance * font->scale.x;
-                        
-                        if (clusterFound)
+                        if (clusterFound && (it->second.font != font))
                         {
-                            it->second.addShape(codepoint, offset, advance);
+                            continue; // CLUSTER FOUND, WITH ANOTHER FONT (E.G. SPACE)
                         }
                         else
                         {
-                            clusterMap.insert(make_pair(cluster, Cluster(font, codepoint, offset, advance)));
+                            auto offset = Vec2f(glyphPositions[i].x_offset, -glyphPositions[i].y_offset) * font->scale;
+                            float advance = glyphPositions[i].x_advance * font->scale.x;
+                            
+                            if (clusterFound)
+                            {
+                                it->second.addShape(codepoint, offset, advance);
+                            }
+                            else
+                            {
+                                clusterMap.insert(make_pair(cluster, Cluster(font, codepoint, offset, advance)));
+                            }
                         }
                     }
+                    else if (!clusterFound)
+                    {
+                        hasMissingGlyphs = true;
+                    }
                 }
-                else if (!clusterFound)
+                
+                if (!hasMissingGlyphs)
                 {
-                    hasMissingGlyphs = true;
+                    break; // NO NEED TO PROCEED TO THE NEXT FONT IN THE LIST
                 }
-            }
-            
-            if (!hasMissingGlyphs)
-            {
-                break; // NO NEED TO PROCEED TO THE NEXT FONT IN THE LIST
             }
         }
         
@@ -163,7 +166,6 @@ LineLayout* VirtualFont::createLineLayout(const TextLine &line)
                 layout->addCluster(it->second);
             }
         }
-
     }
     
     hb_buffer_destroy(buffer);

commit 6b212bff0653acf82f913f39a5cc234d1b8dca11

VirtualFont AND Rendering PROJECTS: FIXING REGRESSION (INTRODUCED IN COMMIT 604c7ab) AFECTING (AT LEAST) HEBREW AND THAI

Projects/Rendering/src/ActualFont.cpp
@@ -100,8 +100,6 @@ void ActualFont::reload()
             throw runtime_error("HARFBUZZ: FONT IS BROKEN OR IRRELEVANT");
         }
         
-        hbFont = hb_ft_font_create(ftFace, NULL);
-
         // ---
         
         /*
@@ -126,6 +124,11 @@ void ActualFont::reload()
         };
         
         FT_Set_Transform(ftFace, &matrix, NULL);
+
+        /*
+         * THIS MUST TAKE PLACE AFTER ftFace IS PROPERLY SCALED AND TRANSFORMED
+         */
+        hbFont = hb_ft_font_create(ftFace, NULL);
         
         // ---
         

Projects/Rendering/src/Sketch.h
@@ -16,6 +16,8 @@
  * 3) THE DIRECTION IS NOW UNDEFINED BY DEFAULT IN TEXT-ITEMS:
  *    - ALLOWS TO DETERMINE "PARAGRAPH-LEVEL" FROM TEXT
  *    - OF COURSE, IT'S ALWAYS BETTER TO SPECIFY A DIRECTION
+ *
+ * 4) FIXING REGRESSION (INTRODUCED IN COMMIT 604c7ab) AFECTING (AT LEAST) HEBREW AND THAI
  */
 
 /*
@@ -27,8 +29,6 @@
 
 /*
  * TODO:
- *
- * 0) FIX PROBLEM WITH THAI DIACRITICS IN HEBREW AND THAI
  *
  * 0) SOLUTION FOR OBTAINING FROM A LineLayout:
  *    - THE "MAX" HEIGHT

Projects/VirtualFont/src/ActualFont.cpp
@@ -100,8 +100,6 @@ void ActualFont::reload()
             throw runtime_error("HARFBUZZ: FONT IS BROKEN OR IRRELEVANT");
         }
         
-        hbFont = hb_ft_font_create(ftFace, NULL);
-
         // ---
         
         /*
@@ -126,6 +124,11 @@ void ActualFont::reload()
         };
         
         FT_Set_Transform(ftFace, &matrix, NULL);
+        
+        /*
+         * THIS MUST TAKE PLACE AFTER ftFace IS PROPERLY SCALED AND TRANSFORMED
+         */
+        hbFont = hb_ft_font_create(ftFace, NULL);
         
         // ---
         

Projects/VirtualFont/src/Sketch.h
@@ -51,6 +51,8 @@
  *    - NECESSARY FOR FONT-FALLBACK IN ORDER TO MATCH SIZE BETWEEN NON PROPORTIONALLY SCALED FONTS
  *
  * 10) USING PREBUILT hb AND icu-common LIBS FOR OSX AND iOS (LIKE ON ANDROID)
+ *
+ * 11) FIXING REGRESSION (INTRODUCED IN COMMIT 604c7ab) AFECTING (AT LEAST) HEBREW AND THAI
  */
 
 /*