9e6969e586e2be4efb387f93274ff91ebfe6b737
Ariel Malka <ariel@chronotext.org>
2014-01-27 14:32:02 +0200

    Rendering PROJECT: LayoutCache::getLineLayout IS NOW RETURNING A CONST REFERENCE

--- Projects/Rendering/src/LayoutCache.cpp
+++ Projects/Rendering/src/LayoutCache.cpp
@@ -16,7 +16,7 @@ capacity(capacity),
 size(0)
 {}
 
-LineLayout* LayoutCache::getLineLayout(VirtualFont *virtualFont, const string &text, const string &langHint, hb_direction_t overallDirection)
+const LineLayout& LayoutCache::getLineLayout(VirtualFont *virtualFont, const string &text, const string &langHint, hb_direction_t overallDirection)
 {
     const LineLayoutKey key(virtualFont, text, langHint, overallDirection);
     auto it = cache.left.find(key);
@@ -27,7 +27,7 @@ LineLayout* LayoutCache::getLineLayout(VirtualFont *virtualFont, const string &t
          * MOVING USED-ENTRY TO THE TAIL OF THE bimaps::list_of
          */
         cache.right.relocate(cache.right.end(), cache.project_right(it));
-        return it->second.get();
+        return *it->second;
     }
     else
     {
@@ -57,7 +57,7 @@ LineLayout* LayoutCache::getLineLayout(VirtualFont *virtualFont, const string &t
         auto value = virtualFont->createLineLayout(text, langHint, overallDirection);
         cache.insert(typename container_type::value_type(key, shared_ptr<LineLayout>(value)));
         
-        return value;
+        return *value;
     }
 }
 
--- Projects/Rendering/src/LayoutCache.h
+++ Projects/Rendering/src/LayoutCache.h
@@ -49,9 +49,9 @@ public:
     LayoutCache(size_t capacity = 32 * 1024);
     
     /*
-     * THE RETURNED INSTANCE IS MANAGED BY LayoutCache AND WILL BE VALID AS LONG AS THE LATTER IS ALIVE
+     * THE RETURNED REFERENCE IS MANAGED BY LayoutCache AND WILL BE VALID AS LONG AS THE LATTER IS ALIVE
      */
-    LineLayout* getLineLayout(VirtualFont *virtualFont, const std::string &text, const std::string &langHint = "", hb_direction_t overallDirection = HB_DIRECTION_INVALID);
+    const LineLayout& getLineLayout(VirtualFont *virtualFont, const std::string &text, const std::string &langHint = "", hb_direction_t overallDirection = HB_DIRECTION_INVALID);
     
     void clear();
 
